79775b028444bd4166ed2723b9fa4404c0e028c4
Ronald Marques <ronaldpmarques01@gmail.com>
2021-12-21 16:29:07 -0300

    Novos conteudos

---
+++ partials/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const path = require('path');
+
+
+let users = [
+
+    {
+        id: 0, 
+        name: 'Igor oliveira',
+        phone: '132456'
+    },
+
+    {
+        id: 1, 
+        name: 'José silva',
+        phone: '789456'
+    },
+    
+    {
+        id: 2, 
+        name: 'Pedro luz',
+        phone: '4568'
+    }
+
+
+]
+
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('user', {users: users}); 
+
+})
+
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+})
+
+
+app.listen(3000, () => {
+
+    console.log('Server running on 3000');
+
+})
